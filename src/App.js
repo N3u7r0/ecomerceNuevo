d8461ab80777dd2dcf3eeebec9781580b8a7ab91
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-09-05 21:52:04 -0300

    commit con itemCount

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Nav from "./componentes/nav/Nav";
 import Header from "./componentes/header/Header";
 import Main from "./componentes/main/Main";
 import Footer from "./componentes/footer/Footer";
-import itemCount from "./componentes/itemCount"
+import ItemCount from "./componentes/itemCount/ItemCount";
 
 function App() {
   return (
     <div>
       <Nav/>
-      <itemCount stock={5} initial={1} onAdd ={0}/>
+      <ItemCount stock={5} initial={1} onAdd ={0}/>
       <Header/>
       <Main/>
       <Footer/>
